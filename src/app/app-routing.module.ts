fada27f9cf0c0c297c0db5f71a84d9809814f97f
Edwin F <lamierdapeluda@hotmail.com>
2023-03-15 15:51:15 -0500

    se agrego iconos, img, y componentes de vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Components/Views/dashboard/dashboard.component';
+import { LoginComponent } from './Components/Views/login/login.component';
+import { RegisterComponent } from './Components/Views/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/Layouts/navbar/navbar.component';
+import { SidebarComponent } from './Components/Layouts/sidebar/sidebar.component';
+import { FooterComponent } from './Components/Layouts/footer/footer.component';
+import { LoginComponent } from './Components/Views/login/login.component';
+import { RegisterComponent } from './Components/Views/register/register.component';
+import { DashboardComponent } from './Components/Views/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
